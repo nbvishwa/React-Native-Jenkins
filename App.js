85fe7df6028b17d74261dda454391ed28ab99db2
nbvishwa <vishwavijet.bagoji@tarento.com>
2018-12-06 14:35:36 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      // <View>
+      //   <Text>Hello World!</Text>
+      //   {/* <Text style={styles.instructions}>To get started, edit App.js</Text>
+      //   <Text style={styles.instructions}>{instructions}</Text> */}
+      // </View>
+
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 1, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 1, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
